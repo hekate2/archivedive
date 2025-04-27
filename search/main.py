ff6b1b249286c1ac63c24a2546086a9a9292973f
hekate2 <howsoonisnow1121@gmail.com>
2025-04-27 00:11:43 -0700

    trashy

--- search/main.py
+++ search/main.py
@@ -1,8 +1,13 @@
 import shutil
 import os
+from dumps import dump_data
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 index_folder = os.path.join(dir_path, "index")
+sites_db = os.path.join(dir_path, "data", "sites.db")
+
+if not os.path.exists(sites_db):
+    dump_data()
 
 if os.path.exists(index_folder):
     shutil.rmtree(index_folder)
